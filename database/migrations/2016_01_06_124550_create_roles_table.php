0766cd42cac526b5966e77aa3af614a1f7b97aac
Chaow Porkaew <pchaowmobile@gmail.com>
2016-01-06 21:15:57 +0700

    migrate migration form successmodel4

--- database/migrations/2016_01_06_124550_create_roles_table.php
+++ database/migrations/2016_01_06_124550_create_roles_table.php
@@ -14,6 +14,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('key')->unique();
             $table->string('name');
             $table->text('description');
             $table->timestamps();
@@ -27,6 +28,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('faculties');
+        Schema::drop('roles');
     }
 }
---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Faculty;
+
+class AdminSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+
+
+	public function run()
+	{
+
+        $user = new \App\Models\User();
+        $user->email="admin@success.local";
+        $user->password =  \Hash::make("admin");
+        $user->save();
+
+        $role = \App\Models\Role::where("key","=","admin")->first();
+        $user->roles()->attach($role->id);
+
+    }
+
+}
