72456fd8dc6ecd7a73104249107e305c4bc1f0af
Chaow Porkaew <pchaowmobile@gmail.com>
2016-01-07 08:12:07 +0700

    add scuffole admin's project and user's project

---
+++ app/Http/Controllers/Backends/AdminProjectController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Backends;
+
+use App\Models\Faculty;
+use App\Models\ProjectStatus;
+use App\Models\Role;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminProjectController extends BaseController
+{
+    //use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        return view("backends.admin-project-index");
+    }
+
+    public function addForm(Request $request)
+    {
+
+    }
+
+    public function doAdd(Request $request)
+    {
+
+    }
+
+    public function editForm(Request $request)
+    {
+
+    }
+
+    public function doEdit(Request $request)
+    {
+
+    }
+
+    public function doDelete(Request $request)
+    {
+
+    }
+
+}
