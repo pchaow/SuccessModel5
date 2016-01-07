2db04613943348b623b3d259d4479f2480c679ba
Chaow Porkaew <pchaowmobile@gmail.com>
2016-01-07 21:24:07 +0700

    add project  model

---
+++ app/Models/Project.php
@@ -0,0 +1,17 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'projects';
+
+
+    protected $fillable = ['name_th','name_en','description_th','description_en'];
+
+}
