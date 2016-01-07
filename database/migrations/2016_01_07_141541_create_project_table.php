c1ebfbb4e6ece7b09b0e4882dbbc4b05f7d2454d
Chaow Porkaew <pchaowmobile@gmail.com>
2016-01-07 21:18:39 +0700

    add migration to create_project_table

---
+++ database/migrations/2016_01_07_141541_create_project_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name_th');
+            $table->string('name_en');
+            $table->string('description_th');
+            $table->string('description_en');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::drop('projects');
+
+    }
+}
