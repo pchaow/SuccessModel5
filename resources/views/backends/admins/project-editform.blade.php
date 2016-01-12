f49ffc575c821f754345b069d93d006fbc51f45b
Chaow Porkaew <pchaowmobile@gmail.com>
2016-01-12 21:15:09 +0700

    add google map drawing

--- resources/views/backends/admins/project-editform.blade.php
+++ resources/views/backends/admins/project-editform.blade.php
@@ -2,9 +2,23 @@
 
 @section('javascript')
 
-    <script src="http://maps.google.com/maps/api/js?sensor=false&libraries=geometry&v=3.22&key=AIzaSyCyb1w6ezK3C0k64_1AiB0vK-qjmQkCrcI"></script>
+    <script src="https://maps.google.com/maps/api/js?sensor=false&libraries=drawing&v=3.22&key=AIzaSyCyb1w6ezK3C0k64_1AiB0vK-qjmQkCrcI"></script>
     <script src="{{ asset("/bower/maplace-js/dist/maplace.min.js") }}"></script>
 
+    <style>
+        #color-palette {
+            clear: both;
+        }
+
+        .color-button {
+            width: 14px;
+            height: 14px;
+            font-size: 0;
+            margin: 2px;
+            float: left;
+            cursor: pointer;
+        }
+    </style>
 @endsection
 
 @section('content')
--- resources/views/backends/admins/project-form.blade.php
+++ resources/views/backends/admins/project-form.blade.php
@@ -12,8 +12,21 @@
 <div class="ui bottom attached tab active" data-tab="first">
     <form class="ui form" action="{{$action}}" method="post">
         {{csrf_field()}}
+
+        <h4 class="ui dividing header">ข้อมูลพื้นฐาน</h4>
+
         <div class="field">
-            <label>กอง/คณะ/วิทยาลัย</label>
+            <label>ชื่อโครงการภาษาไทย</label>
+            <input type="text" name="project[name_th]" placeholder="ชื่อโครงการภาษาไทย" value="{{$project->name_th}}">
+        </div>
+        <div class="field">
+            <label>ชื่อโครงการภาษาอังกฤษ</label>
+            <input type="text" name="project[name_en]" placeholder="ชื่อโครงการภาษาอังกฤษ"
+                   value="{{$project->name_en}}">
+        </div>
+
+        <div class="field">
+            <label>กอง/คณะ/วิทยาลัย ที่ดำเนินโครงการ</label>
             <div class="ui selection dropdown" tabindex="0">
                 <input type="hidden" name="project[faculty][id]" value="{{$project->faculty_id}}">
                 @if($project->faculty_id)
@@ -37,18 +50,9 @@
             </div>
         </div>
 
+        <h4 class="ui dividing header">สถานที่ดำเนินโครงการ</h4>
         <div class="field">
-            <label>ชื่อโครงการภาษาไทย</label>
-            <input type="text" name="project[name_th]" placeholder="ชื่อโครงการภาษาไทย" value="{{$project->name_th}}">
-        </div>
-        <div class="field">
-            <label>ชื่อโครงการภาษาอังกฤษ</label>
-            <input type="text" name="project[name_en]" placeholder="ชื่อโครงการภาษาอังกฤษ"
-                   value="{{$project->name_en}}">
-        </div>
-
-        <div class="field">
-            <label>สถานที่ดำเนินโครงการ</label>
+            <label>ชื่อสถานที่</label>
             <input type="text" name="project[location]" placeholder="สถานที่ดำเนินโครงการ"
                    value="{{$project->location}}">
         </div>
@@ -57,7 +61,7 @@
             <div class="field">
                 <label>จังหวัด</label>
                 <div id="map_dropdown_province" class="ui fluid selection dropdown">
-                    <input type="hidden" name="province_id">
+                    <input type="hidden" name="project[province_id]">
                     <i class="dropdown icon"></i>
                     <div class="default text">เลือกจังหวัด</div>
 
@@ -76,7 +80,7 @@
             <div class="field">
                 <label>อำเภอ</label>
                 <div id="map_dropdown_amphur" class="ui fluid selection dropdown">
-                    <input type="hidden" name="amphur_id">
+                    <input type="hidden" name="project[amphur_id]">
                     <i class="dropdown icon"></i>
                     <div class="default text">เลือกอำเภอ</div>
                     <div class="menu"></div>
@@ -85,13 +89,15 @@
             <div class="field">
                 <label>ตำบล</label>
                 <div id="map_dropdown_district" class="ui fluid selection dropdown">
-                    <input type="hidden" name="district_id">
+                    <input type="hidden" name="project[district_id]">
                     <i class="dropdown icon"></i>
                     <div class="default text">เลือกตำบล</div>
                     <div class="menu"></div>
                 </div>
             </div>
         </div>
+
+        <h4 class="ui dividing header">รายละเอียดโครงการ</h4>
 
         <div class="field">
             <label>รายละเอียดโครงการ ภาษาไทย</label>
@@ -103,6 +109,7 @@
             <textarea name="project[description_en]" rows="10">{{$project->description_en}}</textarea>
         </div>
 
+        <h4 class="ui dividing header">ข้อมูลอื่นๆ สำหรับผู้ดูแลระบบ</h4>
 
         <div class="field">
             <label>สถานะโครงการ</label>
@@ -646,16 +653,170 @@
 <div class="ui bottom attached tab" data-tab="sixth">
 
     <form class="ui form">
-
         <div class="field">
-            <div id="gmap" style="with:300px;height:300px;"></div>
+            <div id="color-palette"></div>
+            <div>
+                <button type="button" id="delete-button">Delete Selected Shape</button>
+            </div>
+        </div>
+        <div class="field">
+            <div id="gmap" style="with:300px;height:600px;"></div>
         </div>
     </form>
 
 </div>
 
 <script>
-    $('.menu .item').tab({});
+
+    var drawingManager;
+    var selectedShape;
+    var colors = ['#1E90FF', '#FF1493', '#32CD32', '#FF8C00', '#4B0082'];
+    var selectedColor;
+    var colorButtons = {};
+
+    function clearSelection() {
+        if (selectedShape) {
+            selectedShape.setEditable(false);
+            selectedShape = null;
+        }
+    }
+
+    function setSelection(shape) {
+        clearSelection();
+        selectedShape = shape;
+        shape.setEditable(true);
+        selectColor(shape.get('fillColor') || shape.get('strokeColor'));
+    }
+
+    function deleteSelectedShape() {
+        if (selectedShape) {
+            selectedShape.setMap(null);
+        }
+    }
+
+    function selectColor(color) {
+        selectedColor = color;
+        for (var i = 0; i < colors.length; ++i) {
+            var currColor = colors[i];
+            colorButtons[currColor].style.border = currColor == color ? '2px solid #789' : '2px solid #fff';
+        }
+
+        // Retrieves the current options from the drawing manager and replaces the
+        // stroke or fill color as appropriate.
+        var polylineOptions = drawingManager.get('polylineOptions');
+        polylineOptions.strokeColor = color;
+        drawingManager.set('polylineOptions', polylineOptions);
+
+        var rectangleOptions = drawingManager.get('rectangleOptions');
+        rectangleOptions.fillColor = color;
+        drawingManager.set('rectangleOptions', rectangleOptions);
+
+        var circleOptions = drawingManager.get('circleOptions');
+        circleOptions.fillColor = color;
+        drawingManager.set('circleOptions', circleOptions);
+
+        var polygonOptions = drawingManager.get('polygonOptions');
+        polygonOptions.fillColor = color;
+        drawingManager.set('polygonOptions', polygonOptions);
+    }
+
+    function setSelectedShapeColor(color) {
+        if (selectedShape) {
+            if (selectedShape.type == google.maps.drawing.OverlayType.POLYLINE) {
+                selectedShape.set('strokeColor', color);
+            } else {
+                selectedShape.set('fillColor', color);
+            }
+        }
+    }
+
+    function makeColorButton(color) {
+        var button = document.createElement('span');
+        button.className = 'color-button';
+        button.style.backgroundColor = color;
+        google.maps.event.addDomListener(button, 'click', function () {
+            selectColor(color);
+            setSelectedShapeColor(color);
+        });
+
+        return button;
+    }
+
+    function buildColorPalette() {
+        var colorPalette = document.getElementById('color-palette');
+        for (var i = 0; i < colors.length; ++i) {
+            var currColor = colors[i];
+            var colorButton = makeColorButton(currColor);
+            colorPalette.appendChild(colorButton);
+            colorButtons[currColor] = colorButton;
+        }
+        selectColor(colors[0]);
+    }
+
+    function initialize() {
+        var map = new google.maps.Map(document.getElementById('gmap'), {
+            zoom: 10,
+            center: new google.maps.LatLng(22.344, 114.048),
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI: true,
+            zoomControl: true
+        });
+
+        var polyOptions = {
+            strokeWeight: 0,
+            fillOpacity: 0.45,
+            editable: true
+        };
+        // Creates a drawing manager attached to the map that allows the user to draw
+        // markers, lines, and shapes.
+        drawingManager = new google.maps.drawing.DrawingManager({
+            drawingMode: google.maps.drawing.OverlayType.POLYGON,
+            markerOptions: {
+                draggable: true
+            },
+            polylineOptions: {
+                editable: true
+            },
+            rectangleOptions: polyOptions,
+            circleOptions: polyOptions,
+            polygonOptions: polyOptions,
+            map: map
+        });
+
+        google.maps.event.addListener(drawingManager, 'overlaycomplete', function (e) {
+            if (e.type != google.maps.drawing.OverlayType.MARKER) {
+                // Switch back to non-drawing mode after drawing a shape.
+                drawingManager.setDrawingMode(null);
+
+                // Add an event listener that selects the newly-drawn shape when the user
+                // mouses down on it.
+                var newShape = e.overlay;
+                newShape.type = e.type;
+                google.maps.event.addListener(newShape, 'click', function () {
+                    setSelection(newShape);
+                });
+                setSelection(newShape);
+            }
+        });
+
+        // Clear the current selection when the drawing mode is changed, or when the
+        // map is clicked.
+        google.maps.event.addListener(drawingManager, 'drawingmode_changed', clearSelection);
+        google.maps.event.addListener(map, 'click', clearSelection);
+        google.maps.event.addDomListener(document.getElementById('delete-button'), 'click', deleteSelectedShape);
+
+        buildColorPalette();
+    }
+
+
+    $('.menu .item').tab({
+        onVisible: function (tab) {
+            if (tab == "sixth") {
+
+                initialize();
+            }
+        }
+    });
 
     $('.menu .item').tab('change tab', "{{$step}}")
 
@@ -663,14 +824,6 @@
     $('form .dropdown')
             .dropdown({})
     ;
-    var maplace = new Maplace();
-    maplace.Load({
-        map_options: {
-            set_center: [19, 99],
-            zoom: 8
-        }
-
-    });
 
 
 </script>
