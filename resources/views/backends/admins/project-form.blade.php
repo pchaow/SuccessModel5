387503590f734cd8131acf274e120dea973916f3
Chaow Porkaew <pchaowmobile@gmail.com>
2016-01-14 15:54:31 +0700

    revert to latest google map form

--- resources/views/backends/admins/project-form.blade.php
+++ resources/views/backends/admins/project-form.blade.php
@@ -50,7 +50,6 @@
             </div>
         </div>
 
-        <h4 class="ui dividing header">สถานที่ดำเนินโครงการ</h4>
         <div class="field">
             <label>ชื่อสถานที่</label>
             <input type="text" name="project[location]" placeholder="สถานที่ดำเนินโครงการ"
@@ -665,6 +664,7 @@
         </div>
 
         <div class="field">
+            <input type="hidden" id="geojson-input"/>
             <button class="ui button" type="button" id="save-mapdata-button">บันทึกข้อมูล</button>
         </div>
     </form>
@@ -674,147 +674,87 @@
 <script>
 
     $("#save-mapdata-button").on('click', function (ev) {
+        console.log(map.data);
+    })
+
+    var map;
+    var geoJsonInput = document.getElementById('geojson-input');
+    var selectedShape;
+
+    function refreshGeoJsonFromData() {
+        map.data.toGeoJson(function (geoJson) {
+            geoJsonInput.value = JSON.stringify(geoJson, null, 2);
+        });
+    }
+
+    function selectedFeature(feature) {
+
+        if (selectedShape != null) {
+            map.data.overrideStyle(selectedShape.feature, {
+                draggable: false,
+                editable: false
+            })
+        }
+
+
+        selectedShape = feature;
+        map.data.overrideStyle(selectedShape.feature, {
+            draggable: true,
+            editable: true
+        })
+    }
+
+    function clearSelection() {
+
+        if (selectedShape != null) {
+            map.data.overrideStyle(selectedShape.feature, {
+                draggable: false,
+                editable: false
+            })
+        }
+
+        selectedShape = null;
+
+    }
+
+    function bindDataLayerListeners(dataLayer) {
+        dataLayer.addListener('addfeature', refreshGeoJsonFromData);
+        dataLayer.addListener('removefeature', refreshGeoJsonFromData);
+        dataLayer.addListener('setgeometry', refreshGeoJsonFromData);
+        dataLayer.addListener("click", selectedFeature);
+
+
+        selectedShape = dataLayer;
+    }
+
+    $("#delete-button").on('click', function (ev) {
+        if (selectedShape != null) {
+            map.data.remove(selectedShape.feature);
+            clearSelection();
+        }
 
     })
 
-    var drawingManager;
-    var selectedShape;
-    var colors = ['#1E90FF', '#FF1493', '#32CD32', '#FF8C00', '#4B0082'];
-    var selectedColor;
-    var colorButtons = {};
-
-    function clearSelection() {
-        if (selectedShape) {
-            selectedShape.setEditable(false);
-            selectedShape = null;
-        }
-    }
-
-    function setSelection(shape) {
-        clearSelection();
-        selectedShape = shape;
-        shape.setEditable(true);
-        selectColor(shape.get('fillColor') || shape.get('strokeColor'));
-    }
-
-    function deleteSelectedShape() {
-        if (selectedShape) {
-            selectedShape.setMap(null);
-        }
-    }
-
-    function selectColor(color) {
-        selectedColor = color;
-        for (var i = 0; i < colors.length; ++i) {
-            var currColor = colors[i];
-            colorButtons[currColor].style.border = currColor == color ? '2px solid #789' : '2px solid #fff';
-        }
-
-        // Retrieves the current options from the drawing manager and replaces the
-        // stroke or fill color as appropriate.
-        var polylineOptions = drawingManager.get('polylineOptions');
-        polylineOptions.strokeColor = color;
-        drawingManager.set('polylineOptions', polylineOptions);
-
-        var rectangleOptions = drawingManager.get('rectangleOptions');
-        rectangleOptions.fillColor = color;
-        drawingManager.set('rectangleOptions', rectangleOptions);
-
-        var circleOptions = drawingManager.get('circleOptions');
-        circleOptions.fillColor = color;
-        drawingManager.set('circleOptions', circleOptions);
-
-        var polygonOptions = drawingManager.get('polygonOptions');
-        polygonOptions.fillColor = color;
-        drawingManager.set('polygonOptions', polygonOptions);
-    }
-
-    function setSelectedShapeColor(color) {
-        if (selectedShape) {
-            if (selectedShape.type == google.maps.drawing.OverlayType.POLYLINE) {
-                selectedShape.set('strokeColor', color);
-            } else {
-                selectedShape.set('fillColor', color);
-            }
-        }
-    }
-
-    function makeColorButton(color) {
-        var button = document.createElement('span');
-        button.className = 'color-button';
-        button.style.backgroundColor = color;
-        google.maps.event.addDomListener(button, 'click', function () {
-            selectColor(color);
-            setSelectedShapeColor(color);
-        });
-
-        return button;
-    }
-
-    function buildColorPalette() {
-        var colorPalette = document.getElementById('color-palette');
-        for (var i = 0; i < colors.length; ++i) {
-            var currColor = colors[i];
-            var colorButton = makeColorButton(currColor);
-            colorPalette.appendChild(colorButton);
-            colorButtons[currColor] = colorButton;
-        }
-        selectColor(colors[0]);
-    }
 
     function initialize() {
-        var map = new google.maps.Map(document.getElementById('gmap'), {
-            zoom: 10,
+
+        map = new google.maps.Map(document.getElementById('gmap'), {
             center: new google.maps.LatLng(19.2178981, 100.1890168),
+            zoom: 10,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
-            disableDefaultUI: true,
             zoomControl: true
         });
 
-        var polyOptions = {
-            strokeWeight: 0,
-            fillOpacity: 0.45,
-            editable: true
-        };
-        // Creates a drawing manager attached to the map that allows the user to draw
-        // markers, lines, and shapes.
-        drawingManager = new google.maps.drawing.DrawingManager({
-            drawingMode: google.maps.drawing.OverlayType.POLYGON,
-            markerOptions: {
-                draggable: true
-            },
-            polylineOptions: {
-                editable: true
-            },
-            rectangleOptions: polyOptions,
-            circleOptions: polyOptions,
-            polygonOptions: polyOptions,
-            map: map
+        map.data.setControls(['Point', 'Polygon']);
+        map.data.setStyle({
+            editable: false,
+            draggable: false
         });
 
-        google.maps.event.addListener(drawingManager, 'overlaycomplete', function (e) {
-            if (e.type != google.maps.drawing.OverlayType.MARKER) {
-                // Switch back to non-drawing mode after drawing a shape.
-                drawingManager.setDrawingMode(null);
 
-                // Add an event listener that selects the newly-drawn shape when the user
-                // mouses down on it.
-                var newShape = e.overlay;
-                newShape.type = e.type;
-                google.maps.event.addListener(newShape, 'click', function () {
-                    setSelection(newShape);
-                });
-                setSelection(newShape);
-            }
-        });
-
-        // Clear the current selection when the drawing mode is changed, or when the
-        // map is clicked.
-        google.maps.event.addListener(drawingManager, 'drawingmode_changed', clearSelection);
         google.maps.event.addListener(map, 'click', clearSelection);
-        google.maps.event.addDomListener(document.getElementById('delete-button'), 'click', deleteSelectedShape);
 
-        buildColorPalette();
+        bindDataLayerListeners(map.data);
     }
 
 
