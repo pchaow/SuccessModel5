a4e84158934a441ca266bb52e6e3c22a3614e4e2
Chaow Porkaew <pchaowmobile@gmail.com>
2016-01-14 21:13:42 +0700

    fix auto column frontend

--- resources/views/frontends/index.blade.php
+++ resources/views/frontends/index.blade.php
@@ -8,95 +8,40 @@
     $colProject2 = $projects->every(3, 1);
     $colProject3 = $projects->every(3, 2);
 
+    $columnsProject = [$colProject1, $colProject2, $colProject3];
     ?>
 
     <div class="ui three columns grid">
-        <div class="column">
-            @foreach($colProject1 as $project)
-                <div class="ui fluid card">
-                    <div class="image">
-                        @if($project->cover_file == "")
-                            <img src="/images/uplogo_big.png">
-                        @else
-                            <img src="/project/{{$project->id}}/cover/{{$project->cover_file}}">
-                        @endif
-                    </div>
-                    <div class="content">
-                        <a class="header">{{$project->name_th}}</a>
-                        <div class="meta">
-                            <span class="faculty">{{$project->faculty->name_th}}</span>
+        @foreach($columnsProject as $col)
+            <div class="column">
+                @foreach($col as $project)
+                    <div class="ui fluid card">
+                        <div class="image">
+                            @if($project->cover_file == "")
+                                <img src="/images/uplogo_big.png">
+                            @else
+                                <img src="/project/{{$project->id}}/cover/{{$project->cover_file}}">
+                            @endif
                         </div>
-                        <div class="description">
-                            {{$project->description_th}}
+                        <div class="content">
+                            <a class="header">{{$project->name_th}}</a>
+                            <div class="meta">
+                                <span class="faculty">{{$project->faculty->name_th}}</span>
+                            </div>
+                            <div class="description">
+                                {{\Illuminate\Support\Str::limit($project->description_th, 255)}}
+                            </div>
                         </div>
-                    </div>
-                    <div class="extra content">
-                        <a>
-                            <i class="user icon"></i>
-                            22 Friends
-                        </a>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-
-        <div class="column">
-            @foreach($colProject2 as $project)
-                <div class="ui fluid card">
-                    <div class="image">
-                        @if($project->cover_file == "")
-                            <img src="/images/uplogo_big.png">
-                        @else
-                            <img src="/project/{{$project->id}}/cover/{{$project->cover_file}}">
-                        @endif
-                    </div>
-                    <div class="content">
-                        <a class="header">{{$project->name_th}}</a>
-                        <div class="meta">
-                            <span class="faculty">{{$project->faculty->name_th}}</span>
-                        </div>
-                        <div class="description">
-                            {{$project->description_th}}
+                        <div class="extra content">
+                            <a>
+                                <i class="user icon"></i>
+                                22 Friends
+                            </a>
                         </div>
                     </div>
-                    <div class="extra content">
-                        <a>
-                            <i class="user icon"></i>
-                            22 Friends
-                        </a>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-        <div class="column">
-            @foreach($colProject3 as $project)
-                <div class="ui fluid card">
-                    <div class="image">
-                        @if($project->cover_file == "")
-                            <img src="/images/uplogo_big.png">
-                        @else
-                            <img src="/project/{{$project->id}}/cover/{{$project->cover_file}}">
-                        @endif
-                    </div>
-                    <div class="content">
-                        <a class="header">{{$project->name_th}}</a>
-                        <div class="meta">
-                            <span class="faculty">{{$project->faculty->name_th}}</span>
-                        </div>
-                        <div class="description">
-                            {{$project->description_th}}
-                        </div>
-                    </div>
-                    <div class="extra content">
-                        <a>
-                            <i class="user icon"></i>
-                            22 Friends
-                        </a>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-
+                @endforeach
+            </div>
+        @endforeach
     </div>
 
 @endsection
