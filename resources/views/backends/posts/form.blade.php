6aa33be929873c282aa3df82192eda4b70dc74f4
Chaow Porkaew <pchaowmobile@gmail.com>
2016-02-04 20:49:52 +0700

    fix typo

--- resources/views/backends/posts/form.blade.php
+++ resources/views/backends/posts/form.blade.php
@@ -55,9 +55,9 @@
         @endif
 
         @if($type == "ADD")
-            <button class="ui button" tabindex="0">บันทักข่าวประกาศใหม่</button>
+            <button class="ui button" tabindex="0">บันทึกข่าวประกาศใหม่</button>
         @else
-            <button class="ui button" tabindex="0">บันทักข่าวประกาศ</button>
+            <button class="ui button" tabindex="0">บันทึกข่าวประกาศ</button>
         @endif
 
         <a href="{{$cancel}}" class="ui red button" tabindex="0">ยกเลิก</a>
